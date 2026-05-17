1c3e498db47881bb137e1fb2df653e91e55c575f
Смутин Даниил <Dsmutin2003@yandex.ru>
2026-05-17 13:04:07 +0300

    В мутацию DoWhileToWhile добавлена фильтрация при наличии операторов continue или break;

--- src/main/java/org/example/neo4j/repository/DoWhileLoopRepository.java
+++ src/main/java/org/example/neo4j/repository/DoWhileLoopRepository.java
@@ -29,6 +29,12 @@ public class DoWhileLoopRepository extends NodeRepository implements AutoCloseab
     public List<DoWhileLoopNode> findAllDoWhileLoops() {
         String cypher = """
                 MATCH (n:DoStatement)
+                WHERE NOT EXISTS {
+                MATCH (stmt)
+                WHERE (stmt:ContinueStatement OR stmt:BreakStatement)
+                AND stmt.startLine > n.startLine
+                AND stmt.endLine < n.endLine
+                }
                 RETURN id(n)          AS id,
                        n.code         AS code,
                        n.startLine    AS startLine,
